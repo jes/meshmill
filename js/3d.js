04e5f759f240e8d3ba4feaf7d516c57fc3353ab7
James Stanley <james@incoherency.co.uk>
2022-04-12 13:34:19 +0100

    Refactor to make it easier to switch to orthographic camera (because the camera needs to know the object bounding box in order to set the viewport)

--- js/3d.js
+++ js/3d.js
@@ -1,5 +1,3 @@
-STLViewer("/home/jes/cad/sake-set/insert.stl");
-
 var container;
 var camera;
 var renderer;
@@ -7,8 +5,6 @@ var renderer;
 function STLViewer(model) {
     container = document.getElementById('scene');
     while(container.firstChild) container.removeChild(container.firstChild);
-
-    camera = new THREE.PerspectiveCamera(70, container.clientWidth/container.clientHeight, 1, 1000);
 
     renderer = new THREE.WebGLRenderer({
         antialias: true,
@@ -18,21 +14,6 @@ function STLViewer(model) {
     renderer.setPixelRatio(window.devicePixelRatio);
     container.appendChild(renderer.domElement);
 
-    window.addEventListener('resize', onWindowResize, false);
-    onWindowResize();
-
-    var controls = new THREE.OrbitControls(camera, renderer.domElement);
-    controls.enableDamping = true;
-    controls.rotateSpeed = 0.25;
-    controls.dampingFactor = 0.99;
-    controls.enableZoom = true;
-    controls.autoRotate = false;
-
-    var scene = new THREE.Scene();
-    scene.add(new THREE.AmbientLight(0xffffff, 0.25));
-    camera.add(new THREE.DirectionalLight(0xffffff, 0.75));
-    scene.add(camera);
-
     (new THREE.STLLoader()).load(model, function (geometry) {
         var material = new THREE.MeshPhongMaterial({ 
             color: 0x0cf4c7,
@@ -40,15 +21,32 @@ function STLViewer(model) {
             specular: 1,
         });
         var mesh = new THREE.Mesh(geometry, material);
-        scene.add(mesh);
 
         var middle = new THREE.Vector3();
         geometry.computeBoundingBox();
         geometry.boundingBox.getCenter(middle);
         mesh.geometry.applyMatrix(new THREE.Matrix4().makeTranslation(-middle.x, -middle.y, -middle.z));
 
+        /* TODO: try to use an orthographic camera (but it seems like
+         * lights don't work if they're attached to an orthographic
+         * camera?)
+        camera = new THREE.PerspectiveCamera(70, container.clientWidth/container.clientHeight, 1, 1000);
+
+        var scene = new THREE.Scene();
+        scene.add(mesh);
+        scene.add(new THREE.AmbientLight(0xffffff, 0.25));
+        camera.add(new THREE.DirectionalLight(0xffffff, 0.75));
+        scene.add(camera);
+
         var largestDimension = Math.max(geometry.boundingBox.max.x, geometry.boundingBox.max.y, geometry.boundingBox.max.z);
-        camera.position.z = largestDimension * 1.5;
+        camera.position.z = largestDimension * 2;
+
+        var controls = new THREE.OrbitControls(camera, renderer.domElement);
+        controls.enableDamping = true;
+        controls.rotateSpeed = 0.25;
+        controls.dampingFactor = 0.99;
+        controls.enableZoom = true;
+        controls.autoRotate = false;
 
         var animate = function () {
             requestAnimationFrame(animate);
@@ -57,8 +55,12 @@ function STLViewer(model) {
         }; 
 
         animate();
+
+        onWindowResize();
     });
 }
+
+window.addEventListener('resize', onWindowResize, false);
 
 function onWindowResize() {
     renderer.setSize(0,0);
